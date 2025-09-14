254c85ce62caa3cd2237eb72d5d12d55737291e2
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-09-14 23:22:10 +0300

    added function adding new car into base

--- my_iveco_crm/clients/views.py
+++ my_iveco_crm/clients/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Client, Truck
-from .serializers import ClientSerializer, TruckSerializer
+from .models import Client, Truck, IvecoBaseModel # Додаємо IvecoBaseModel
+# Імпортуємо всі нові серіалізатори
+from .serializers import ClientSerializer, TruckListSerializer, TruckDetailSerializer, IvecoBaseModelSerializer
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
+# Створюємо ViewSet для довідника моделей, він нам знадобиться у формі
+class IvecoBaseModelViewSet(viewsets.ModelViewSet):
+    queryset = IvecoBaseModel.objects.all()
+    serializer_class = IvecoBaseModelSerializer
+
 class TruckViewSet(viewsets.ModelViewSet):
     queryset = Truck.objects.all()
-    serializer_class = TruckSerializer
-# Create your views here.
+
+    # Ця функція дозволяє нам вибирати серіалізатор в залежності від дії
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TruckListSerializer # Для списку
+        return TruckDetailSerializer # Для всього іншого (створення, редагування)
