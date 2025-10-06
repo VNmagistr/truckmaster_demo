960f072b4f662dfa397a038be0d428b818d21370
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-10-07 00:02:48 +0300

    fix for migrations

--- my_iveco_crm/orders/serializers.py
+++ my_iveco_crm/orders/serializers.py
@@ -4,73 +4,8 @@ from inventory.models import UsedPart
 from clients.serializers import ClientSerializer, TruckListSerializer
 from inventory.serializers import PartSerializer
 
-# --- НОВІ СЕРІАЛІЗАТОРИ ---
-class WorkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Work
-        fields = ['id', 'name', 'price']
+# --- СПОЧАТКУ ВИЗНАЧАЄМО ВСІ ДОПОМІЖНІ СЕРІАЛІЗАТОРИ ---
 
-class WorkCategorySerializer(serializers.ModelSerializer):
-    works = WorkSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = WorkCategory
-        fields = ['id', 'name', 'works']
-
-# --- ОНОВЛЕНІ СЕРІАЛІЗАТОРИ ---
-class ServiceWorkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ServiceWork
-        # Фронтенд надсилає ID роботи з прайсу, опис і вартість (яка може бути змінена)
-        fields = ['id', 'work', 'custom_description', 'cost']
-
-class ServiceOrderWriteSerializer(serializers.ModelSerializer):
-    works = ServiceWorkSerializer(many=True)
-
-    class Meta:
-        model = ServiceOrder
-        fields = ['id', 'client', 'truck', 'status', 'start_date', 'works']
-
-    def create(self, validated_data):
-        works_data = validated_data.pop('works')
-        order = ServiceOrder.objects.create(**validated_data)
-        for work_data in works_data:
-            ServiceWork.objects.create(service_order=order, **work_data)
-        return order
-
-# --- Серіалізатори для читання (оновлені відповідно до нових моделей) ---
-class ServiceWorkDetailSerializer(serializers.ModelSerializer):
-    work = WorkSerializer(read_only=True) # Показуємо повну інформацію про роботу
-    employee = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = ServiceWork
-        fields = ['id', 'work', 'custom_description', 'cost', 'employee']
-
-class ServiceOrderDetailSerializer(serializers.ModelSerializer):
-    client = ClientSerializer(read_only=True)
-    truck = TruckListSerializer(read_only=True)
-    status = serializers.CharField(source='get_status_display')
-    works = ServiceWorkDetailSerializer(many=True, read_only=True)
-    repair_photos = RepairPhotoSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = ServiceOrder
-        fields = [
-            'id', 'client', 'truck', 'status', 'start_date', 'end_date', 
-            'total_cost', 'works', 'repair_photos', 'car_photo', 'odometer_photo'
-        ]
-
-class ServiceOrderListSerializer(serializers.ModelSerializer):
-    client = serializers.StringRelatedField(read_only=True)
-    truck = serializers.StringRelatedField(read_only=True)
-    status = serializers.CharField(source='get_status_display')
-
-    class Meta:
-        model = ServiceOrder
-        fields = ['id', 'truck', 'client', 'status', 'start_date', 'total_cost']
-
-# --- Існуючі серіалізатори, які залишаються без змін ---
 class RepairPhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = RepairPhoto
@@ -86,3 +21,75 @@ class UsedPartSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsedPart
         fields = ['id', 'part', 'quantity']
+
+class WorkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Work
+        fields = ['id', 'name', 'price']
+
+class WorkCategorySerializer(serializers.ModelSerializer):
+    works = WorkSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = WorkCategory
+        fields = ['id', 'name', 'works']
+
+class ServiceWorkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceWork
+        # Фронтенд надсилає ID роботи з прайсу, опис і вартість (яка може бути змінена)
+        fields = ['id', 'work', 'custom_description', 'cost', 'employee']
+
+
+# --- ТЕПЕР ВИЗНАЧАЄМО ОСНОВНІ ("КОМПОЗИТНІ") СЕРІАЛІЗАТОРИ ---
+
+# Серіалізатор для запису (створення/оновлення)
+class ServiceOrderWriteSerializer(serializers.ModelSerializer):
+    works = ServiceWorkSerializer(many=True)
+
+    class Meta:
+        model = ServiceOrder
+        fields = ['id', 'client', 'truck', 'status', 'start_date', 'works']
+
+    def create(self, validated_data):
+        works_data = validated_data.pop('works')
+        order = ServiceOrder.objects.create(**validated_data)
+        for work_data in works_data:
+            ServiceWork.objects.create(service_order=order, **work_data)
+        return order
+
+# Серіалізатор для читання (список)
+class ServiceOrderListSerializer(serializers.ModelSerializer):
+    client = serializers.StringRelatedField(read_only=True)
+    truck = serializers.StringRelatedField(read_only=True)
+    status = serializers.CharField(source='get_status_display')
+
+    class Meta:
+        model = ServiceOrder
+        fields = ['id', 'truck', 'client', 'status', 'start_date', 'total_cost']
+
+# Окремий детальний серіалізатор для вкладених робіт
+class ServiceWorkDetailSerializer(serializers.ModelSerializer):
+    work = WorkSerializer(read_only=True)
+    employee = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = ServiceWork
+        fields = ['id', 'work', 'custom_description', 'cost', 'employee']
+
+# Серіалізатор для читання (детальна сторінка)
+class ServiceOrderDetailSerializer(serializers.ModelSerializer):
+    client = ClientSerializer(read_only=True)
+    truck = TruckListSerializer(read_only=True)
+    status = serializers.CharField(source='get_status_display')
+    # Тепер ServiceWorkDetailSerializer визначено вище, і помилки не буде
+    works = ServiceWorkDetailSerializer(many=True, read_only=True)
+    # Тепер RepairPhotoSerializer визначено вище, і помилки не буде
+    repair_photos = RepairPhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ServiceOrder
+        fields = [
+            'id', 'client', 'truck', 'status', 'start_date', 'end_date', 
+            'total_cost', 'works', 'repair_photos', 'car_photo', 'odometer_photo'
+        ]
