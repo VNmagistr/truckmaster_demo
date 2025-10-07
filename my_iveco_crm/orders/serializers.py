603582491a49c3459e1785daf72fe9b27b643070
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-10-07 21:57:08 +0300

    changed: serializers.py

--- my_iveco_crm/orders/serializers.py
+++ my_iveco_crm/orders/serializers.py
@@ -1,10 +1,12 @@
+# orders/serializers.py
+
 from rest_framework import serializers
 from .models import ServiceOrder, ServiceWork, Employee, Work, WorkCategory, RepairPhoto
 from inventory.models import UsedPart
 from clients.serializers import ClientSerializer, TruckListSerializer
 from inventory.serializers import PartSerializer
 
-# --- СПОЧАТКУ ВИЗНАЧАЄМО ВСІ ДОПОМІЖНІ СЕРІАЛІЗАТОРИ ---
+# --- 1. СПОЧАТКУ ВИЗНАЧАЄМО ВСІ "ПРОСТІ" ТА ДОПОМІЖНІ СЕРІАЛІЗАТОРИ ---
 
 class RepairPhotoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,11 +24,13 @@ class UsedPartSerializer(serializers.ModelSerializer):
         model = UsedPart
         fields = ['id', 'part', 'quantity']
 
+# Серіалізатор для однієї роботи з прайс-листа
 class WorkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Work
-        fields = ['id', 'name', 'price']
+        fields = ['id', 'name', 'price_per_hour'] # Використовуємо 'price_per_hour'
 
+# Серіалізатор для Категорії робіт (включає вкладений список робіт)
 class WorkCategorySerializer(serializers.ModelSerializer):
     works = WorkSerializer(many=True, read_only=True)
 
@@ -34,18 +38,18 @@ class WorkCategorySerializer(serializers.ModelSerializer):
         model = WorkCategory
         fields = ['id', 'name', 'works']
 
-class ServiceWorkSerializer(serializers.ModelSerializer):
+# Серіалізатор для ОДНІЄЇ виконаної роботи (для ЗАПИСУ в замовлення)
+class ServiceWorkWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceWork
-        # Фронтенд надсилає ID роботи з прайсу, опис і вартість (яка може бути змінена)
-        fields = ['id', 'work', 'custom_description', 'cost', 'employee']
+        # Фронтенд надсилає ID роботи, опис і кількість годин
+        fields = ['id', 'work', 'custom_description', 'duration_hours', 'employee']
 
+# --- 2. ТЕПЕР ВИЗНАЧАЄМО ОСНОВНІ ("КОМПОЗИТНІ") СЕРІАЛІЗАТОРИ ---
 
-# --- ТЕПЕР ВИЗНАЧАЄМО ОСНОВНІ ("КОМПОЗИТНІ") СЕРІАЛІЗАТОРИ ---
-
-# Серіалізатор для запису (створення/оновлення)
+# Серіалізатор для ЗАПИСУ (створення/оновлення) замовлення
 class ServiceOrderWriteSerializer(serializers.ModelSerializer):
-    works = ServiceWorkSerializer(many=True)
+    works = ServiceWorkWriteSerializer(many=True)
 
     class Meta:
         model = ServiceOrder
@@ -56,9 +60,17 @@ class ServiceOrderWriteSerializer(serializers.ModelSerializer):
         order = ServiceOrder.objects.create(**validated_data)
         for work_data in works_data:
             ServiceWork.objects.create(service_order=order, **work_data)
+        # Можна додати виклик update_total_cost після створення, якщо потрібно
+        # order.update_total_cost() 
         return order
+        
+    def update(self, instance, validated_data):
+        # Логіка для оновлення замовлення та його робіт (за потреби)
+        # ...
+        return super().update(instance, validated_data)
 
-# Серіалізатор для читання (список)
+
+# Серіалізатор для ЧИТАННЯ (список замовлень)
 class ServiceOrderListSerializer(serializers.ModelSerializer):
     client = serializers.StringRelatedField(read_only=True)
     truck = serializers.StringRelatedField(read_only=True)
@@ -68,23 +80,21 @@ class ServiceOrderListSerializer(serializers.ModelSerializer):
         model = ServiceOrder
         fields = ['id', 'truck', 'client', 'status', 'start_date', 'total_cost']
 
-# Окремий детальний серіалізатор для вкладених робіт
+# Окремий детальний серіалізатор для вкладених робіт (для ЧИТАННЯ)
 class ServiceWorkDetailSerializer(serializers.ModelSerializer):
     work = WorkSerializer(read_only=True)
     employee = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = ServiceWork
-        fields = ['id', 'work', 'custom_description', 'cost', 'employee']
+        fields = ['id', 'work', 'custom_description', 'duration_hours', 'cost', 'employee']
 
-# Серіалізатор для читання (детальна сторінка)
+# Серіалізатор для ЧИТАННЯ (детальна сторінка замовлення)
 class ServiceOrderDetailSerializer(serializers.ModelSerializer):
     client = ClientSerializer(read_only=True)
     truck = TruckListSerializer(read_only=True)
     status = serializers.CharField(source='get_status_display')
-    # Тепер ServiceWorkDetailSerializer визначено вище, і помилки не буде
     works = ServiceWorkDetailSerializer(many=True, read_only=True)
-    # Тепер RepairPhotoSerializer визначено вище, і помилки не буде
     repair_photos = RepairPhotoSerializer(many=True, read_only=True)
 
     class Meta:
