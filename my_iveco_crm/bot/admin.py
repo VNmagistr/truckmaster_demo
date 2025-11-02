555a92b9367d40f107310a5bf9309b0aa91ff3d9
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-11-02 22:58:23 +0200

    Add BotMessageLog to admin panel

--- my_iveco_crm/bot/admin.py
+++ my_iveco_crm/bot/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import BotMessageLog
 
-# Register your models here.
+@admin.register(BotMessageLog)
+class BotMessageLogAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'chat_id', 'message_text', 'bot_response', 'created_at')
+    list_filter = ('user_name', 'created_at')
+    search_fields = ('user_name', 'message_text', 'bot_response')
+    readonly_fields = ('created_at',) # Поля, які не можна редагувати
+
+    def has_add_permission(self, request):
+        return False # Забороняємо створювати логи вручну
+
+    def has_change_permission(self, request, obj=None):
+        return False # Забороняємо редагувати логи
