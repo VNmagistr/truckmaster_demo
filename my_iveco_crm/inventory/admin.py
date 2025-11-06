3b816520cb1588a15765d8f72b1e56a052d5839d
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-11-06 22:20:39 +0200

    dd substitutes M2M field to Part model

--- my_iveco_crm/inventory/admin.py
+++ my_iveco_crm/inventory/admin.py
@@ -3,26 +3,24 @@ from .models import PartCategory, Part, UsedPart
 
 @admin.register(PartCategory)
 class PartCategoryAdmin(admin.ModelAdmin):
-    # Додаємо 'parent' у список
     list_display = ('name', 'parent', 'description')
-    list_filter = ('parent',) # Додаємо фільтр за батьківською категорією
+    list_filter = ('parent',) 
     search_fields = ('name',)
-    # Дозволяє легкий пошук батьківської категорії
     autocomplete_fields = ['parent'] 
 
 @admin.register(Part)
 class PartAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'sku_code', 'selling_price', 'current_stock')
     search_fields = ('name', 'sku_code')
-    # Оновлюємо фільтр, щоб можна було фільтрувати і за батьком
     list_filter = ('category__parent', 'category') 
     list_editable = ('selling_price', 'current_stock')
-    # Дозволяє легкий пошук категорії
     autocomplete_fields = ['category'] 
+
+    # 👇 ДОДАЄМО ЦЕЙ РЯДОК 👇
+    # Це створить зручний інтерфейс "вибору" для поля 'substitutes'
+    filter_horizontal = ('substitutes',)
 
 @admin.register(UsedPart)
 class UsedPartAdmin(admin.ModelAdmin):
     list_display = ('service_work', 'part', 'quantity')
-    # autocomplete_fields потрібні для зручного пошуку
-    # у зв'язаних моделях (особливо коли їх багато)
     autocomplete_fields = ['service_work', 'part']
