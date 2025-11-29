1d640a75bf50ace77bad23297926519c701e5b1a
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-11-29 20:56:03 +0200

    fix: clean urls.py and update inventory API

--- my_iveco_crm/my_iveco_crm/urls.py
+++ my_iveco_crm/my_iveco_crm/urls.py
@@ -8,13 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-<<<<<<< HEAD
-    # API endpoints
-    path('api/', include('accounts.urls')),
-    path('api/', include('clients.urls')),
-    path('api/', include('orders.urls')),
-    path('api/inventory/', include('inventory.urls')),
-=======
     # Аутентифікація
     path('api/', include('accounts.urls')),
     
@@ -26,7 +19,6 @@ urlpatterns = [
     path('api/inventory/', include('inventory.urls')),
     
     # Обслуговування
->>>>>>> a1f17255c6788a0df72d1230f982c97e1a0d302d
     path('api/maintenance/', include('maintenance.urls')),
 ]
 
