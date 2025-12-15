cfbb9f5ecebf1b0f521001880d257fff025c5cc1
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-12-15 21:12:40 +0200

    fixed bot/urls.py

--- my_iveco_crm/bot/urls.py
+++ my_iveco_crm/bot/urls.py
@@ -1,15 +1,12 @@
-# bot/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BotUserViewSet, MessageLogViewSet, ReminderSettingsViewSet
 
 router = DefaultRouter()
-router.register(r'users', BotUserViewSet, basename='bot-user')
-router.register(r'messages', MessageLogViewSet, basename='message-log')
-router.register(r'reminders', ReminderSettingsViewSet, basename='reminder-settings')
+router.register(r'users', BotUserViewSet)
+router.register(r'messages', MessageLogViewSet)
+router.register(r'reminders', ReminderSettingsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/bot/', include('bot.urls')),
 ]
