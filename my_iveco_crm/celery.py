42f70ea292c4948b1a5b973c0b13f21455cbdac2
Volodymyr Nos <nosvolodymyr@gmail.com>
2025-12-16 11:22:28 +0200

    Add Celery and Redis for reminders

---
+++ my_iveco_crm/celery.py
@@ -0,0 +1,30 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+# Встановлюємо Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_iveco_crm.settings')
+
+app = Celery('my_iveco_crm')
+
+# Завантажуємо конфіг з Django settings
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Автоматично знаходимо tasks в додатках
+app.autodiscover_tasks()
+
+# Періодичні задачі
+app.conf.beat_schedule = {
+    'send-daily-reminders': {
+        'task': 'bot.tasks.send_daily_reminders',
+        'schedule': crontab(hour=9, minute=0),  # Щодня о 9:00
+    },
+    'check-maintenance-reminders': {
+        'task': 'bot.tasks.check_maintenance_reminders',
+        'schedule': crontab(hour='*/6'),  # Кожні 6 годин
+    },
+}
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
