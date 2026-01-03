bb5980a4f6f7cebc78c8b70b28d7e75b15b1b0d2
Volodymyr Nos <nosvolodymyr@gmail.com>
2026-01-03 21:43:17 +0200

    Add truck search endpoint for license plate autocomplete

--- my_iveco_crm/clients/views.py
+++ my_iveco_crm/clients/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from .models import Client, Truck, IvecoBaseModel
 from .serializers import ClientSerializer, TruckListSerializer, TruckDetailSerializer, IvecoBaseModelSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
@@ -13,10 +15,35 @@ class IvecoBaseModelViewSet(viewsets.ModelViewSet):
 
 class TruckViewSet(viewsets.ModelViewSet):
     queryset = Truck.objects.select_related('client', 'base_model').all()
+    serializer_class = TruckSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['client']
 
     def get_serializer_class(self):
         if self.action == 'list':
             return TruckListSerializer
-        return TruckDetailSerializer
+        return TruckSerializer
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        """Пошук автомобілів за частиною номерного знаку"""
+        query = request.query_params.get('license_plate', '')
+        
+        if len(query) < 2:
+            return Response({'results': []})
+        
+        trucks = Truck.objects.filter(
+            license_plate__icontains=query
+        ).select_related('client')[:10]
+        
+        results = [{
+            'id': truck.id,
+            'license_plate': truck.license_plate,
+            'vin_code': truck.vin_code,
+            'last_seven_vin': truck.last_seven_vin,
+            'specific_model_name': truck.specific_model_name,
+            'client_id': truck.client_id,
+            'client_name': truck.client.name if truck.client else 'Невідомий клієнт',
+        } for truck in trucks]
+        
+        return Response({'results': results})
