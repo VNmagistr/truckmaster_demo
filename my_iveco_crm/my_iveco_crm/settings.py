753f17b0ee8612914b1191176e71aee129741438
Volodymyr Nos <nosvolodymyr@gmail.com>
2026-01-03 19:32:23 +0200

    Allow larger page_size for pagination - increase MAX_PAGE_SIZE to 10000

--- my_iveco_crm/my_iveco_crm/settings.py
+++ my_iveco_crm/my_iveco_crm/settings.py
@@ -166,6 +166,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
+    'MAX_PAGE_SIZE': 10000
 }
 
 # Налаштування для медіафайлів (завантажених користувачами)
