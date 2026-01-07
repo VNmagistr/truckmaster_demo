59a61efba565636104ec69dfd9a32e08d116cde4
Volodymyr Nos <nosvolodymyr@gmail.com>
2026-01-07 12:11:06 +0200

    Fix truck search: use exact license plate match instead of contains

--- my_iveco_crm/clients/views.py
+++ my_iveco_crm/clients/views.py
@@ -1,47 +1,39 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
 from .models import Client, Truck, IvecoBaseModel
-from .serializers import ClientSerializer, TruckListSerializer, IvecoBaseModelSerializer
-
+from .serializers import ClientSerializer, TruckListSerializer, TruckDetailSerializer, IvecoBaseModelSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
-
-class TruckViewSet(viewsets.ModelViewSet):
-    queryset = Truck.objects.select_related('client', 'base_model').all()
-    serializer_class = TruckListSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['client']
-
-    @action(detail=False, methods=['get'])
-    def search(self, request):
-        """Пошук автомобілів за частиною номерного знаку"""
-        query = request.query_params.get('license_plate', '')
-        
-        if len(query) < 2:
-            return Response({'results': []})
-        
-        trucks = Truck.objects.filter(
-            license_plate__icontains=query
-        ).select_related('client')[:10]
-        
-        results = [{
-            'id': truck.id,
-            'license_plate': truck.license_plate,
-            'full_vin': truck.full_vin,
-            'last_seven_vin': truck.last_seven_vin,
-            'specific_model_name': truck.specific_model_name,
-            'client_id': truck.client_id,
-            'client_name': truck.client.name if truck.client else 'Невідомий клієнт',
-        } for truck in trucks]
-        
-        return Response({'results': results})
-
-
 class IvecoBaseModelViewSet(viewsets.ModelViewSet):
     queryset = IvecoBaseModel.objects.all()
     serializer_class = IvecoBaseModelSerializer
+
+class TruckViewSet(viewsets.ModelViewSet):
+    queryset = Truck.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['client']
+    search_fields = ['license_plate', 'last_seven_vin']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TruckListSerializer
+        return TruckDetailSerializer
+    
+    def get_queryset(self):
+        """
+        Фільтрація вантажівок з точним співпадінням по номеру
+        """
+        queryset = super().get_queryset()
+        
+        # Точний пошук по номеру (license_plate)
+        license_plate = self.request.query_params.get('license_plate', None)
+        if license_plate:
+            # Точне співпадіння, без урахування регістру
+            queryset = queryset.filter(license_plate__iexact=license_plate)
+        
+        return queryset
+
