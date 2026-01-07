e9ebabc3ec793a92670381e7d8b45ef548ffe70e
Volodymyr Nos <nosvolodymyr@gmail.com>
2026-01-07 20:11:55 +0200

    Add user info and role to JWT token response

--- my_iveco_crm/accounts/serializers.py
+++ my_iveco_crm/accounts/serializers.py
@@ -9,6 +9,31 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
         token['username'] = user.username
         return token
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        # Додаємо інформацію про користувача
+        data['user'] = {
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+        }
+        
+        # Додаємо роль з UserProfile (якщо є)
+        if hasattr(self.user, 'profile'):
+            data['user']['role'] = self.user.profile.role
+            data['user']['position'] = self.user.profile.position
+        else:
+            # Якщо профілю немає, перевіряємо чи це superuser
+            if self.user.is_superuser:
+                data['user']['role'] = 'admin'
+            else:
+                data['user']['role'] = 'mechanic'  # За замовчуванням
+        
+        return data
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
